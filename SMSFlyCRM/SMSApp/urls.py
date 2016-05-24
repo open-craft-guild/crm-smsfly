0d54a1f2a99defa47eeb943d3c350df2b5b20dff
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2016-05-24 08:43:33 +0300

    Fix urls map

--- SMSFlyCRM/SMSApp/urls.py
+++ SMSFlyCRM/SMSApp/urls.py
@@ -4,22 +4,22 @@ from . import views
 
 
 urlpatterns = (
-    # Index page with a menu:
-    url('', views.IndexView.as_view(), name='app-root'),
     # Alphanames list:
-    url('alphanames', views.AlphanameIndexView.as_view(), name='alphanames-root'),
+    url('^alphanames', views.AlphanameIndexView.as_view(), name='alphanames-root'),
     # Alphaname registration request form:
-    url('alphanames/new', views.AlphanameRegisterView.as_view(), name='add-alphaname'),
+    url('^alphanames/new', views.AlphanameRegisterView.as_view(), name='add-alphaname'),
     # Active campaigns list:
-    url('sms-campaigns', views.CampaignIndexView.as_view(), name='campaigns-root'),
+    url('^sms-campaigns', views.CampaignIndexView.as_view(), name='campaigns-root'),
     # Campaign adding/scheduling form:
-    url('sms-campaigns/new', views.CampaignNewView.as_view(), name='add-campaign'),
+    url('^sms-campaigns/new', views.CampaignNewView.as_view(), name='add-campaign'),
     # Campaign editing form:
-    url('sms-campaigns/edit', views.CampaignEditView.as_view(), name='edit-campaign'),
+    url('^sms-campaigns/edit', views.CampaignEditView.as_view(), name='edit-campaign'),
     # Archive of inactive/sent campaigns:
-    url('sms-campaigns/archive', views.CampaignArchiveView.as_view(), name='campaigns-archive'),
+    url('^sms-campaigns/archive', views.CampaignArchiveView.as_view(), name='campaigns-archive'),
     # List of all sent messages:
-    url('sms-campaigns/messages', views.CampaignMessagesView.as_view(), name='campaigns-messages'),
+    url('^sms-campaigns/messages', views.CampaignMessagesView.as_view(), name='campaigns-messages'),
     # Stats on campaigns:
-    url('sms-campaigns/stats', views.CampaignStatsView.as_view(), name='campaigns-stats'),
+    url('^sms-campaigns/stats', views.CampaignStatsView.as_view(), name='campaigns-stats'),
+    # Index page with a menu:
+    url('^', views.IndexView.as_view(), name='app-root'),  # Keep it in bottom
 )

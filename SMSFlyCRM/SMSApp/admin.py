5a02dc5e2a3b1d73a1d7897d4924df9490323fa9
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2016-05-29 17:47:41 +0300

    Add all models into admin panel

--- SMSFlyCRM/SMSApp/admin.py
+++ SMSFlyCRM/SMSApp/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Area, Building, Region, Locality, Street, Project, ProjectContact, FollowerContact, Candidate, FollowerCandidate,
+    PollPlace, FamilyStatus, Education, SocialCategory, Sex, FollowerStatus, Follower,
+    Campaign, Task, Alphaname, Message
+)
+
+# External CRM views
+admin.site.register(Area)
+admin.site.register(Building)
+admin.site.register(Region)
+admin.site.register(Locality)
+admin.site.register(Street)
+admin.site.register(Project)
+admin.site.register(ProjectContact)
+admin.site.register(FollowerContact)
+admin.site.register(Candidate)
+admin.site.register(FollowerCandidate)
+admin.site.register(PollPlace)
+admin.site.register(FamilyStatus)
+admin.site.register(Education)
+admin.site.register(SocialCategory)
+admin.site.register(Sex)
+admin.site.register(FollowerStatus)
+admin.site.register(Follower)
+
+# Our app's models
+admin.site.register(Campaign)
+admin.site.register(Task)
+admin.site.register(Alphaname)
+admin.site.register(Message)

09be57b40ca024a23148b0e9542d10608cf2c08e
Anna Kurylo <anna.kurilo@outlook.com>
2016-06-01 03:19:28 +0300

    Fix middleware

--- SMSFlyCRM/SMSApp/middleware/iframe.py
+++ SMSFlyCRM/SMSApp/middleware/iframe.py
@@ -1,5 +1,8 @@
 class UserInIframeMiddleware:
     """Intercepts user_id from GET request and puts it into session dict"""
     def process_request(self, request):
-        """Set crm_user_id key into session. Fallback to -1"""
-        request.session['crm_user_id'] = request.GET.get('crm_user_id', -1)
+        """Set crm_user_id key into session if passed"""
+        try:
+            request.session['crm_user_id'] = int(request.GET['crm_user_id'])
+        except KeyError:
+            pass  # Don't reset value if already set

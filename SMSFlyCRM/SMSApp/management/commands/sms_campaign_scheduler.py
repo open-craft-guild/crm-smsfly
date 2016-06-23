a4aea5e34f44d5f9183258978c85e20fb0fbc0b7
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2016-06-24 02:02:11 +0300

    Add django command for triggering campaigns scheduler

---
+++ SMSFlyCRM/SMSApp/management/commands/sms_campaign_scheduler.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from django.core.management.base import BaseCommand
+
+from SMSFlyCRM.SMSApp.tasks import scheduleRecurringCampaignTasksFor
+
+
+class Command(BaseCommand):
+    help = 'Schedules campaign sending for specified interval'
+
+    def add_arguments(self, parser):
+        parser.add_argument('min_interval', type=int)
+
+    def handle(self, *args, **options):
+        min_interval = options['min_interval']
+        scheduleRecurringCampaignTasksFor.delay(min_interval)
+        self.stdout.write(self.style.SUCCESS(
+            'Campaign scheduler has been executed at {}'.format(datetime.now())))

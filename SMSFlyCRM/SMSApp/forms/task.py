57a0fa4b273e3a06bcadb05361cd764955883154
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2016-07-11 00:18:31 +0300

    Make tasks filters ready for editing

--- SMSFlyCRM/SMSApp/forms/task.py
+++ SMSFlyCRM/SMSApp/forms/task.py
@@ -42,64 +42,33 @@ class TaskForm(forms.ModelForm):
                               bootstrap_version=3))
 
     def __init__(self, request, *args, **kwargs):
+        self._request = request
+        user_id = self.crm_user_id
+
+        self._extmeta = getattr(self.__class__, 'ExtMeta')
+
+        if callable(self._extmeta):
+            self._extmeta = self._extmeta()
+
         super().__init__(*args, **kwargs)
-        user_id = request.session['crm_user_id']
+
+        try:
+            # Pre-populating filter fields with data extracted from JSON
+            recipients_filter_json = kwargs['instance'].recipients_filter_json
+            if not recipients_filter_json.get('to_everyone'):
+                self.data = self.data.copy()
+                for k, v in recipients_filter_json.items():
+                    setattr(kwargs['instance'], 'y', v)
+                    key = k[:-3] if k.endswith('_id') else k
+                    self.data[key] = v
+        except KeyError:
+            pass
+
+        self.initialize_filter_fields()
 
         self.initial['created_by_crm_user_id'] = user_id
         self.initial['type'] = 0
         self.initial['state'] = 0
-
-        self.fields['to_everyone'] = forms.BooleanField(
-            label=_('Send to all electors'), required=False)
-        self.fields['age_from'] = forms.IntegerField(
-            label=_('Age from'), min_value=0, required=False)
-        self.fields['age_to'] = forms.IntegerField(
-            label=_('Age to'), min_value=0, required=False)
-        self.fields['regaddress_region'] = forms.ModelChoiceField(
-            label=_('Region'), queryset=Region.objects.for_user(user_id).all(), required=False)
-        self.fields['regaddress_area'] = Follower.regaddress_area.field.formfield(
-            label=_('Area'), queryset=Area.objects.for_user(user_id).all(),
-            required=False
-        )
-        self.fields['regaddress_locality'] = Follower.regaddress_locality.field.formfield(
-            label=_('Locality'), queryset=Locality.objects.for_user(user_id).all(),
-            required=False)
-        self.fields['regaddress_street'] = Follower.regaddress_street.field.formfield(
-            label=_('Street'), queryset=Street.objects.for_user(user_id).all(), required=False)
-        self.fields['regaddress_building'] = Follower.regaddress_building.field.formfield(
-            label=_('Building'), queryset=Building.objects.for_user(user_id).all(), required=False)
-        self.fields['address_region'] = forms.ModelChoiceField(
-            label=_('Region'), queryset=Region.objects.for_user(user_id).all(), required=False)
-        self.fields['address_area'] = Follower.address_area.field.formfield(
-            label=_('Area'), queryset=Area.objects.for_user(user_id).all(), required=False)
-        self.fields['address_locality'] = Follower.address_locality.field.formfield(
-            label=_('Locality'), queryset=Locality.objects.for_user(user_id).all(),
-            required=False)
-        self.fields['address_street'] = Follower.address_street.field.formfield(
-            label=_('Street'), queryset=Street.objects.for_user(user_id).all(), required=False)
-        self.fields['address_building'] = Follower.address_building.field.formfield(
-            label=_('Building'), queryset=Building.objects.for_user(user_id).all(), required=False)
-        self.fields['sex'] = forms.ModelChoiceField(
-            label=_('Gender'), queryset=Sex.objects.for_user(user_id).all(), required=False)
-        self.fields['family_status'] = forms.ModelChoiceField(
-            label=_('Family status'), queryset=FamilyStatus.objects.for_user(user_id).all(),
-            required=False)
-        self.fields['education'] = forms.ModelChoiceField(
-            label=_('Education'), queryset=Education.objects.for_user(user_id).all(), required=False)
-        self.fields['social_category'] = forms.ModelChoiceField(
-            label=_('Social Category'), queryset=SocialCategory.objects.for_user(user_id).all(),
-            required=False)
-        self.fields['poll_place'] = forms.ModelChoiceField(
-            label=_('Poll place'), queryset=PollPlace.objects.for_user(user_id).all(),
-            required=False)
-        self.fields['contact'] = forms.ModelChoiceField(
-            label=_('Contact'), queryset=ProjectContact.objects.for_user(user_id).all(),
-            required=False)
-        self.fields['candidate'] = forms.ModelChoiceField(
-            label=_('Candidate'), queryset=Candidate.objects.for_user(user_id).all(),
-            required=False)
-        self.fields['status'] = forms.ModelChoiceField(
-            label=_('Status'), queryset=FollowerStatus.objects.for_user(user_id).all(), required=False)
 
         for field in self.fields.values():
             field.error_messages.update({
@@ -136,6 +105,23 @@ class TaskForm(forms.ModelForm):
 
         return task
 
+    def initialize_filter_fields(self):
+        user_id = self.crm_user_id
+        for fld in self._extmeta.FILTER_FIELDS:
+            if 'get_queryset' in fld:
+                fld['kwargs']['queryset'] = fld['get_queryset'](user_id)
+            self.fields[fld['name']] = fld['field_class'](
+                initial=self.data.get(fld['name']), required=False,
+                **fld.get('kwargs', {}))
+
+    @property
+    def request(self):
+        return self._request
+
+    @property
+    def crm_user_id(self):
+        return self._request.session['crm_user_id']
+
     class Meta:
         model = Task
         fields = ['alphaname', 'title', 'message_text', 'start_datetime', 'type',
@@ -160,6 +146,195 @@ class TaskForm(forms.ModelForm):
             'type': forms.HiddenInput(),
             'state': forms.HiddenInput(),
         }
+
+    class ExtMeta:
+        FILTER_FIELDS = (
+            {
+                'name': 'to_everyone',
+                'field_class': forms.BooleanField,
+                'kwargs': {
+                    'label': _('Send to all electors'),
+                },
+            },
+            {
+                'name': 'age_from',
+                'field_class': forms.IntegerField,
+                'kwargs': {
+                    'label': _('Age from'),
+                    'min_value': 0,
+                },
+            },
+            {
+                'name': 'age_to',
+                'field_class': forms.IntegerField,
+                'kwargs': {
+                    'label': _('Age to'),
+                    'min_value': 0,
+                },
+            },
+            {
+                'name': 'regaddress_region',
+                'field_class': forms.ModelChoiceField,
+                'get_queryset': (lambda user_id:
+                                 Region.objects.for_user(user_id).all()),
+                'kwargs': {
+                    'label': _('Region'),
+                },
+            },
+            {
+                'name': 'regaddress_area',
+                'field_class': Follower.regaddress_area.field.formfield,
+                'get_queryset': (lambda user_id:
+                                 Area.objects.for_user(user_id).all()),
+                'kwargs': {
+                    'label': _('Area'),
+                },
+            },
+            {
+                'name': 'regaddress_locality',
+                'field_class': Follower.regaddress_locality.field.formfield,
+                'get_queryset': (lambda user_id:
+                                 Locality.objects.for_user(user_id).all()),
+                'kwargs': {
+                    'label': _('Locality'),
+                },
+            },
+            {
+                'name': 'regaddress_street',
+                'field_class': Follower.regaddress_street.field.formfield,
+                'get_queryset': (lambda user_id:
+                                 Street.objects.for_user(user_id).all()),
+                'kwargs': {
+                    'label': _('Street'),
+                },
+            },
+            {
+                'name': 'regaddress_building',
+                'field_class': Follower.regaddress_building.field.formfield,
+                'get_queryset': (lambda user_id:
+                                 Building.objects.for_user(user_id).all()),
+                'kwargs': {
+                    'label': _('Building'),
+                },
+            },
+            {
+                'name': 'address_region',
+                'field_class': forms.ModelChoiceField,
+                'get_queryset': (lambda user_id:
+                                 Region.objects.for_user(user_id).all()),
+                'kwargs': {
+                    'label': _('Region'),
+                },
+            },
+            {
+                'name': 'address_area',
+                'field_class': Follower.address_area.field.formfield,
+                'get_queryset': (lambda user_id:
+                                 Area.objects.for_user(user_id).all()),
+                'kwargs': {
+                    'label': _('Area'),
+                },
+            },
+            {
+                'name': 'address_locality',
+                'field_class': Follower.address_locality.field.formfield,
+                'get_queryset': (lambda user_id:
+                                 Locality.objects.for_user(user_id).all()),
+                'kwargs': {
+                    'label': _('Locality'),
+                },
+            },
+            {
+                'name': 'address_street',
+                'field_class': Follower.address_street.field.formfield,
+                'get_queryset': (lambda user_id:
+                                 Street.objects.for_user(user_id).all()),
+                'kwargs': {
+                    'label': _('Street'),
+                },
+            },
+            {
+                'name': 'address_building',
+                'field_class': Follower.address_building.field.formfield,
+                'get_queryset': (lambda user_id:
+                                 Building.objects.for_user(user_id).all()),
+                'kwargs': {
+                    'label': _('Building'),
+                },
+            },
+            {
+                'name': 'sex',
+                'field_class': forms.ModelChoiceField,
+                'get_queryset': (lambda user_id:
+                                 Sex.objects.for_user(user_id).all()),
+                'kwargs': {
+                    'label': _('Gender'),
+                },
+            },
+            {
+                'name': 'family_status',
+                'field_class': forms.ModelChoiceField,
+                'get_queryset': (lambda user_id:
+                                 FamilyStatus.objects.for_user(user_id).all()),
+                'kwargs': {
+                    'label': _('Family status'),
+                },
+            },
+            {
+                'name': 'education',
+                'field_class': forms.ModelChoiceField,
+                'get_queryset': (lambda user_id:
+                                 Education.objects.for_user(user_id).all()),
+                'kwargs': {
+                    'label': _('Education'),
+                },
+            },
+            {
+                'name': 'social_category',
+                'field_class': forms.ModelChoiceField,
+                'get_queryset': (lambda user_id:
+                                 SocialCategory.objects.for_user(user_id).all()),
+                'kwargs': {
+                    'label': _('Social Category'),
+                },
+            },
+            {
+                'name': 'poll_place',
+                'field_class': forms.ModelChoiceField,
+                'get_queryset': (lambda user_id:
+                                 PollPlace.objects.for_user(user_id).all()),
+                'kwargs': {
+                    'label': _('Poll place'),
+                },
+            },
+            {
+                'name': 'contact',
+                'field_class': forms.ModelChoiceField,
+                'get_queryset': (lambda user_id:
+                                 ProjectContact.objects.for_user(user_id).all()),
+                'kwargs': {
+                    'label': _('Contact'),
+                },
+            },
+            {
+                'name': 'candidate',
+                'field_class': forms.ModelChoiceField,
+                'get_queryset': (lambda user_id:
+                                 Candidate.objects.for_user(user_id).all()),
+                'kwargs': {
+                    'label': _('Candidate'),
+                },
+            },
+            {
+                'name': 'status',
+                'field_class': forms.ModelChoiceField,
+                'get_queryset': (lambda user_id:
+                                 FollowerStatus.objects.for_user(user_id).all()),
+                'kwargs': {
+                    'label': _('Status'),
+                },
+            },
+        )
 
 
 class OneTimeTaskForm(TaskForm):
@@ -208,7 +383,7 @@ class EventDrivenTaskForm(TaskForm):
 
     def __init__(self, request, *args, **kwargs):
         super().__init__(request, *args, **kwargs)
-        user_id = request.session['crm_user_id']
+        user_id = self.crm_user_id
         self.initial['type'] = 2
 
         self.fields['touch_project'] = forms.ModelChoiceField(
